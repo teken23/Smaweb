db4938a5f6d3f7210660e61253ac19d1e3ca486b
teken23 <android.0737@gmail.com>
2025-11-13 12:08:32 -0400

    Quitar outputFileTracingRoot para Vercel

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },

42ba1685f67f5a5fa9c96f3b55a2b69996d870c4
teken23 <android.0737@gmail.com>
2025-11-13 11:56:53 -0400

    Arreglo distDir para Vercel

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
   output: process.env.NEXT_OUTPUT_MODE,
   experimental: {
     outputFileTracingRoot: path.join(__dirname, '../'),
